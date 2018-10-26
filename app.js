53dd2397e59eeda6d236c6aa452185fe0cb036dd
JMITCH18 <ronmitchell1@yahoo.com>
2018-10-25 19:10:23 -0500

    Setting up Mongoose

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const express = require("express"),
   mongoose = require("mongoose");
 
 // Setting up Mongoose
+mongoose.connect("mongodb://localhost/restFul_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// title
+// image
+// body
+// created
