ad12fbf1e20b57de97c71a7ab94c8f4f17493f47
JMITCH18 <ronmitchell1@yahoo.com>
2018-10-25 19:32:13 -0500

    Added listening port

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express"),
   app = express(),
-  bodyParser = require(bodyParser),
+  bodyParser = require("body-parser"),
   mongoose = require("mongoose");
 
 // Setting up Mongoose
-mongoose.connect("mongodb://localhost/restFul_blog_app");
+mongoose.connect(
+  "mongodb://localhost/restFul_blog_app",
+  { useNewUrlParser: true }
+);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
