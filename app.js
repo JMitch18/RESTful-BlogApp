2a58dd0831e6ba2cadc268a9117a4fefdaf007e5
JMITCH18 <ronmitchell1@yahoo.com>
2018-10-27 17:53:22 -0500

    Added show route & show page

--- app.js
+++ app.js
@@ -56,6 +56,17 @@ app.post("/blogs", (req, res) => {
     }
   });
 });
+
+// SHOW ROUTE
+app.get("/blogs/:id", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog });
+    }
+  });
+});
 // Port
 app.listen(3000, () => {
   console.log("Listening on port 3000....");
